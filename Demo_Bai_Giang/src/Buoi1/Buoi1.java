f33251f9066419807dd419cc119a78f27758cb63
linlin <tranthaolinh97@gmail.com>
2024-05-02 00:10:33 +0700

    Chuong trinh java dau tien

---
+++ Demo_Bai_Giang/src/Buoi1/Buoi1.java
@@ -0,0 +1,11 @@
+package Buoi1;
+
+public class Buoi1 {
+    public static void main (String[] agrs){
+        System.out.print("Output buoi 1");
+        System.out.print("Cai dat JDK");
+        System.out.print("Cai dat IJ");
+        System.out.print("Tao duoc project git va gui link");
+    }
+
+}
